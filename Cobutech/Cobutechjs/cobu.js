1662411167dc30dd33553670d86301811d3554b6
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-17 19:35:33 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- Cobutech/Cobutechjs/cobu.js
+++ Cobutech/Cobutechjs/cobu.js
@@ -1,40 +1,13 @@
 // Cobutechjs/cobu.js
-const profileContainer = document.querySelector('.profile-container');
-const typingAnimationContainer = document.querySelector('.typing-animation-container');
-const topTextElement = document.querySelector('.top-text');
-const bottomTextElement = document.querySelector('.bottom-text');
-const textTop = "COBUTECH WEB";
-const textBottom = "INDUSTRY POWER ⚡";
-const typingSpeed = 100; // milliseconds per character
-const appearanceDelay = 2000; // 2 seconds
-const redirectionDelay = 4000; // 4 seconds
+const backgroundImage = document.querySelector('.background-image');
+const appearanceDelay = 1000; // 2 seconds
+const redirectionDelay = 3000; // 4 seconds
 
-let topTextIndex = 0;
-let bottomTextIndex = 0;
-
-function typeText(element, text, index, callback) {
-    if (index < text.length) {
-        element.textContent = text.substring(0, index + 1);
-        setTimeout(() => typeText(element, text, index + 1, callback), typingSpeed);
-    } else if (callback) {
-        callback();
-    }
-}
-
-function startAnimation() {
-    typeText(topTextElement, textTop, 0, () => {
-        typeText(bottomTextElement, textBottom, 0, () => {
-            // After the animation, set a timeout for redirection
-            setTimeout(() => {
-                window.location.href = 'Cobutechhtml/inlet.html';
-            }, redirectionDelay);
-        });
-    });
-}
-
-// Make the profile and animation visible and start the animation after the delay
+// Make the background image visible after the delay
 setTimeout(() => {
-    profileContainer.classList.add('visible');
-    typingAnimationContainer.classList.add('visible');
-    startAnimation();
+    backgroundImage.classList.add('visible');
+    // Redirect to inlet.html after a further delay
+    setTimeout(() => {
+        window.location.href = 'Cobutechhtml/inlet.html';
+    }, redirectionDelay);
 }, appearanceDelay);
