0fcf0a4d6a38cc4f13ceed2e1cc9cff2705c47ed
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-18 16:39:10 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- Cobutechserver/server.js
+++ Cobutechserver/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./db');
+const db = require('./cobudb');
 const bcrypt = require('bcrypt');
 
 const app = express();
