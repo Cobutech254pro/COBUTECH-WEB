cf531a41418ff8133c5d27c9c5837b9a4f617b11
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-22 11:19:49 -0700

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- Cobutechserver/cobudb.js
+++ Cobutechserver/cobudb.js
@@ -1,14 +1,11 @@
-// Cobutechserver/cobudb.js
 require('dotenv').config();
 const { Pool } = require('pg');
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false, // Recommended for secure connections
+        rejectUnauthorized: false,
     },
 });
-
 module.exports = {
     query: (text, params) => pool.query(text, params),
     getClient: () => pool.connect(),
