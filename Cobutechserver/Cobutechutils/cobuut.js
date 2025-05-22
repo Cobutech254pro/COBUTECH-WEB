d348e79aa360fe4abf581330da5ac039802cbaa3
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-22 11:21:50 -0700

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- Cobutechserver/Cobutechutils/cobuut.js
+++ Cobutechserver/Cobutechutils/cobuut.js
@@ -1,9 +1,7 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config(); 
-
 const sendVerificationEmail = async (email, verificationCode) => {
     try {
-        
         const transporter = nodemailer.createTransport({
             service: process.env.EMAIL_SERVICE, 
             auth: {
@@ -11,7 +9,6 @@ const sendVerificationEmail = async (email, verificationCode) => {
                 pass: process.env.EMAIL_PASSWORD, 
             },
         });
-
         const mailOptions = {
             from: process.env.EMAIL_USER, 
             to: email, 
@@ -21,16 +18,12 @@ const sendVerificationEmail = async (email, verificationCode) => {
                    <p>Please enter this code on the verification page to activate your account.</p>
                    <p>This code will expire in 2 minutes.</p>`,
         };
-
-        // Send the email
         const info = await transporter.sendMail(mailOptions);
         console.log('Verification email sent:', info.messageId);
         return true; 
-
     } catch (error) {
         console.error('Error sending verification email:', error);
         return false; 
     }
 };
-
 module.exports = sendVerificationEmail;
