2c6e5311d993ce03d7cb0e5c8f0c0fdd24de366f
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-24 23:47:35 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- Cobutech/Cobutechjs/cobu.js
+++ Cobutech/Cobutechjs/cobu.js
@@ -1,11 +1,6 @@
-// Cobutechjs/cobu.js
 const backgroundImage = document.querySelector('.background-image');
-const redirectionDelay = 4000; // 4 seconds
-
-// Make the background image visible immediately
+const redirectionDelay = 4000; 
 backgroundImage.classList.add('visible');
-
-// Redirect to inlet.html after a delay
 setTimeout(() => {
     window.location.href = 'Cobutech/Cobutechhtml/cobuin.html';
 }, redirectionDelay);
